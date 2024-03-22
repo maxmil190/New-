cc68c76e1b053ed6f0f61b7dcf797d913a73ac46
maxmil190 <141026876+maxmil190@users.noreply.github.com>
2024-03-22 08:40:18 -0400

    Update Identity.fs

--- Identity.fs
+++ Identity.fs
@@ -1,1 +1,8 @@
 
+precision mediump float;
+varying highp vec2 qt_TexCoord0;
+uniform sampler2D uTexture;
+void main() {
+    lowp vec4 tex = texture2D(uTexture, qt_TexCoord0);
+    gl_FragColor = tex;
+}
